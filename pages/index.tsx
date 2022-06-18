718d9715182d65a7ce44117b969608fe85140dd3
Vlad <vlvete1990@yandex.ru>
2022-06-18 16:42:34 +0300

    chore: pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-// Home
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
